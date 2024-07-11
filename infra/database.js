59a39c930551f0cd3d5c6f498a41974a946bbc9c
LuanHenriqueGarcia <luangarciatodero2007@gmail.com>
2024-07-11 12:58:13 +0000

    add `.env`

---
+++ infra/database.js
@@ -0,0 +1,34 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Error executing query:", error.stack);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+
+  await client.connect();
+  return client;
+}
+
+export default {
+  query,
+  getNewClient,
+};
