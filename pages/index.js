caa0b737cc849b1b0ff00e28da532525bc7bb794
Luan garcia <luangarciatodero2007@gmail.com>
2024-07-31 09:46:00 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-  return <h1> Olá seja bem vindo! Essa pagina encontra-se desativa... 🐬</h1>;
+  return <h1> Olá seja bem vindoaaaas! Essa pagina encontra-se desativa... 🐬</h1>;
 }
 export default Home;
