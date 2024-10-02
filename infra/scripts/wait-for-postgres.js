751731271fcefc1e475832390123bbc86271d773
LuanHenriqueGarcia <luangarciatodero2007@gmail.com>
2024-10-02 23:08:27 +0000

    adds `linting` GitHub action

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,16 +1,16 @@
-const { exec } = require("node:child_process")
+const { exec } = require("node:child_process");
 
 function checkPostgres() {
-  exec('docker exec postgres-dev pg_isready --host localhost', HandleReturn)
+  exec("docker exec postgres-dev pg_isready --host localhost", HandleReturn);
 
   function HandleReturn(error, stdout) {
-    if(stdout.search("accepting connections") === -1){
-    process.stdout.write(".");   
-    checkPostgres();
-    return;
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
     }
 
-    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n")
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
   }
 }
 
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,9 @@
 import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
-beforeAll( async () => {
+beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;")
+  await database.query("drop schema public cascade; create schema public;");
 });
 
 test("GET to /api/v1/migrations should return 200", async () => {
@@ -11,23 +11,19 @@ test("GET to /api/v1/migrations should return 200", async () => {
     method: "POST",
   });
   expect(response1.status).toBe(201);
-  
+
   const response1Body = await response1.json();
 
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
 
-  
-const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-  method: 'POST'
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
-expect(response2.status).toBe(200);
-
-const response2Body = await response2.json();
-
-expect(Array.isArray(response1Body)).toBe(true);
-expect(response2Body.length).toBe(0);
-
-
-
-}); 
